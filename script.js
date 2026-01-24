84871802f1f9b9bd0648cbaf5eb9e3024132a5ac
JuanPablo140025 <julianelprositoxd@gmail.com>
2026-01-24 18:41:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const verde = document.getElementById("verde");
 const btnRojo = document.getElementById("btn-rojo");
 const btnAmarillo = document.getElementById("btn-amarillo");
 const btnVerde = document.getElementById("btn-verde");
+const btnApagar = document.getElementById("btn-apagar");
+
+const estado = document.getElementById("estado");
 
 // Al cargar la página: todos apagados
 rojo.style.background = "gray";
@@ -15,16 +18,26 @@ btnRojo.addEventListener("click", () => {
     rojo.style.background = "red";
     amarillo.style.background = "gray";
     verde.style.background = "gray";
+    estado.textContent = "Estado: Encendido";
 });
 
 btnAmarillo.addEventListener("click", () => {
     rojo.style.background = "gray";
     amarillo.style.background = "yellow";
     verde.style.background = "gray";
+    estado.textContent = "Estado: Encendido";
 });
 
 btnVerde.addEventListener("click", () => {
     rojo.style.background = "gray";
     amarillo.style.background = "gray";
     verde.style.background = "green";
+    estado.textContent = "Estado: Encendido";
+});
+
+btnApagar.addEventListener("click", () => {
+    rojo.style.background = "gray";
+    amarillo.style.background = "gray";
+    verde.style.background = "gray";
+    estado.textContent = "Estado: Apagado";
 });
